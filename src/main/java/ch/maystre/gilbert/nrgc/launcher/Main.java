b853df3bf4e050b7a6a51c31aa30a47c2bdbba1c
Gilbert Maystre <gilbert@Gilberts-MacBook-Pro.local>
2017-12-18 23:20:07 +0100

    Naive implementation of an all algorithm that finds all the simple paths.

--- src/main/java/ch/maystre/gilbert/nrgc/launcher/Main.java
+++ src/main/java/ch/maystre/gilbert/nrgc/launcher/Main.java
@@ -4,6 +4,7 @@ import ch.maystre.gilbert.nrgc.datastructures.Graph;
 import ch.maystre.gilbert.nrgc.io.Log;
 import ch.maystre.gilbert.nrgc.io.Parser;
 import ch.maystre.gilbert.nrgc.io.Reader;
+import ch.maystre.gilbert.nrgc.pathfinders.GeneralPathFinder;
 import ch.maystre.gilbert.nrgc.pathfinders.TreePathsFinder;
 import ch.maystre.gilbert.nrgc.solvers.ThueIndexSolver;
 import ch.maystre.gilbert.nrgc.solvers.ThueNumberSolver;
@@ -15,35 +16,50 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            String path = "/Users/gilbert/Desktop/Tree6/tree6.3.txt";
-            String fst = (new Reader(path)).readNextLines(1).get(0);
-            Graph tree = Parser.parseTree(fst);
+        //try {
+            //String path = "/Users/gilbert/Desktop/Tree22/tree22.5.txt";
+            //String fst = (new Reader(path)).readNextLines(6).get(5);
+            //Graph tree = Parser.parseTree(fst);
+
+            String graph = "5 9  9 8  8 7  7 6  6 5  0 5  4 9  3 8  2 7  1 6  0 3  0 2  1 4  1 3  2 4"; //petersen graph
+            Graph g = Parser.parseTree(graph);
+            GeneralPathFinder gpf = new GeneralPathFinder(g);
+            Log.print(gpf.findAllPaths());
+
+
+            /*
+            Graph lg = g.getLineGraph();
+            ThueNumberSolver solver = new ThueNumberSolver(lg);
+            solver.addSeparatePaths();
+            Log.print(solver.compute());
+            */
+
+
+
 
             /*
              * compute the Thue index
              */
-            List<List<Integer>> paths = TreePathsFinder.allPaths(tree);
-            ThueIndexSolver edgeSolver = new ThueIndexSolver(tree, paths);
-            Log.print(edgeSolver.compute());
+            //List<List<Integer>> paths = TreePathsFinder.allPaths(tree);
+            //ThueIndexSolver edgeSolver = new ThueIndexSolver(tree, paths);
+            //Log.print(edgeSolver.compute());
 
             /*
              * compute the Thue number
              */
-            ThueNumberSolver vertexSolver = new ThueNumberSolver(tree);
-            Log.print(vertexSolver.addSeparatePaths().compute());
+            //ThueNumberSolver vertexSolver = new ThueNumberSolver(tree);
+            //Log.print(vertexSolver.addSeparatePaths().compute());
 
             /*
              * compute the Line graph
              */
-            Log.print("gap = " + treeGap(tree));
+            //Log.print("gap = " + treeGap(tree));
 
-        } catch (IOException e) {
-            e.printStackTrace();
 
-        } catch (GRBException e) {
+
+        /*} catch (GRBException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public static int treeGap(Graph tree) throws GRBException {
---
+++ src/main/java/ch/maystre/gilbert/nrgc/pathfinders/GeneralPathFinder.java
@@ -0,0 +1,57 @@
+package ch.maystre.gilbert.nrgc.pathfinders;
+
+import ch.maystre.gilbert.nrgc.datastructures.Graph;
+
+import java.util.*;
+
+public class GeneralPathFinder {
+
+    private final Graph graph;
+
+    public GeneralPathFinder(Graph graph){
+        this.graph = graph;
+
+    }
+
+    public List<List<Integer>> findAllPaths(){
+        List<List<Integer>> toReturn = new ArrayList<>();
+        for(int s = 0; s < graph.getN(); s++){
+            for(int t = s + 1; t < graph.getN(); t++){
+                toReturn.addAll(allStPaths(s, t));
+            }
+        }
+        return toReturn;
+    }
+
+    private List<List<Integer>> allStPaths(int s, int t){
+        List<List<Integer>> r = rAllStPaths(s, t, Collections.singleton(s));
+        for(List<Integer> path : r){
+            path.add(s);
+        }
+        return r;
+    }
+
+    private List<List<Integer>> rAllStPaths(int s, int t, Set<Integer> visited){
+        List<List<Integer>> toReturn = new ArrayList<>();
+        for(int v : graph.neighborsOf(s)){
+            if(!visited.contains(v)){
+                if(v == t){
+                    ArrayList<Integer> x = new ArrayList<>();
+                    x.add(t);
+                    toReturn.add(x);
+                }
+                else{
+                    HashSet<Integer> nVisited = new HashSet<>(visited);
+                    nVisited.add(v);
+                    List<List<Integer>> paths = rAllStPaths(v, t, nVisited);
+                    for(List<Integer> path : paths){
+                        path.add(v);
+                        toReturn.add(path);
+                    }
+                }
+            }
+        }
+        return toReturn;
+    }
+
+}
